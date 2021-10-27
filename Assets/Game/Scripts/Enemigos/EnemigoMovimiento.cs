309134327a7f3193a6a1a411f278193d214ac88a
Rafael <rafa.alberti21@gmail.com>
2021-10-27 10:30:35 -0300

    Movimientos enemigos

--- Assets/Game/Scripts/Enemigos/EnemigoMovimiento.cs
+++ Assets/Game/Scripts/Enemigos/EnemigoMovimiento.cs
@@ -6,16 +6,48 @@ public class EnemigoMovimiento : MonoBehaviour
 {
     [SerializeField]EnemigoController enemigoController;
     [SerializeField] private Rigidbody2D rgb2d;
+    [SerializeField, Range(1, 120)] float velocidad = 5.0f;
+    float LimiteDer;
+    float LimiteIzq;
+    int Direccion = 1;
 
     void Start()
     {
         this.rgb2d = GetComponent<Rigidbody2D>();
         this.enemigoController = GetComponent<EnemigoController>();
+        LimiteDer = transform.position.x + GetComponent<CircleCollider2D>().radius;
+        LimiteIzq = transform.position.x - GetComponent<CircleCollider2D>().radius;
     }
 
-    
+
     void Update()
     {
-        
+
+        Caminar();
+
+
     }
+
+
+    public void Caminar()
+    {
+        rgb2d.velocity = new Vector2(velocidad * Direccion, rgb2d.velocity.y);
+        Orientacion();
+    }
+
+    public void Orientacion()
+    {
+        if (transform.position.x < LimiteIzq)
+        {
+            Direccion = 1;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        if (transform.position.x > LimiteDer)
+        {
+            Direccion = -1;
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+
 }
