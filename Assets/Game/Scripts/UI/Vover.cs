41e619e13655aec65a6e8a8a593ebdedd5fae791
Rafael <rafa.alberti21@gmail.com>
2021-10-31 19:20:25 -0300

    Arreglando merge

--- Assets/Game/Scripts/UI/Vover.cs
+++ Assets/Game/Scripts/UI/Vover.cs
@@ -6,4 +6,19 @@ using UnityEngine.SceneManagement;
 public class Vover : MonoBehaviour
 {
 
+    [SerializeField] string NombreNivel;
+    public void VolverAlJuego()
+    {
+        SceneManager.LoadScene(NombreNivel);
+        Time.timeScale = 1;
+        Muerte.MuertePersonaje = false;
+    }
+
+    public void VolverAlInicio()
+    {
+        SceneManager.LoadScene("MenuPrincipal");
+        Time.timeScale = 1;
+        Muerte.MuertePersonaje = false;
+    }
+
 }
