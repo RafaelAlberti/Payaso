15e4bdfb8a2a7cdc332146bfe621309b11955abc
Rafael <rafa.alberti21@gmail.com>
2021-11-01 18:15:47 -0300

    Mejoras en los mapas

---
+++ Assets/Game/Scripts/Objetos/Nivel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Nivel : MonoBehaviour
+{
+
+    [SerializeField] private string nombreDelNivel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.gameManager.Nivel();
+            SceneManager.LoadScene(nombreDelNivel,LoadSceneMode.Single);
+        }
+    }
+}
