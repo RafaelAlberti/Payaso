08fedb36b31621c954e92792fa68889a0e7258b0
Rafael <rafa.alberti21@gmail.com>
2021-11-04 00:11:08 -0300

    Cinematicas

---
+++ Assets/Game/Scripts/UI/Perdiste.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Perdiste : MonoBehaviour
+{
+    public string nombreNivel;
+
+    public void Reiniciar()
+    {
+        SceneManager.LoadScene(nombreNivel, LoadSceneMode.Single);
+        GameManager.gameManager.Nivel();
+    }
+
+
+    public void Salir()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuPrincipal", LoadSceneMode.Single);
+    }
+}
